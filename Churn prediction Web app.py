ecad29f318cef3f7b303cc4e4cd97b6833318c97
Ajay3736pandey <113659194+Ajay3736pandey@users.noreply.github.com>
2023-03-06 12:42:44 +0530

    Update Churn prediction Web app.py

--- Churn prediction Web app.py
+++ Churn prediction Web app.py
@@ -10,7 +10,7 @@ import pickle
 import streamlit as st 
 
  # Loading the saved model 
-loaded_model = pickle.load(open("trained_model.sav",'rb'))
+loaded_model = pickle.load(open("https://github.com/Ajay3736pandey/Churn-Prediction/blob/main/trained_model.sav",'rb'))
 
 
 # creating the function for prediction 
