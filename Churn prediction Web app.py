c983dda1b56344efae4e23c01133b17de7ded8be
Ajay3736pandey <113659194+Ajay3736pandey@users.noreply.github.com>
2023-03-06 14:00:40 +0530

    Update Churn prediction Web app.py

--- Churn prediction Web app.py
+++ Churn prediction Web app.py
@@ -5,77 +5,71 @@ Created on Wed Mar  1 18:41:17 2023
 @author: ajay3
 """
 
-import numpy as np 
-import pickle 
-import pandas as pd 
-from PIL import Image
+import numpy as np
+import pickle
+import pandas as pd
+
 import streamlit as st 
 
- # Loading the saved model 
-loaded_model = pickle.load(open("https://github.com/Ajay3736pandey/Churn-Prediction/blob/main/trained_model.pkl",'rb'))
+from PIL import Image
+
+pickle_in = open("trained_model","rb")
+classifier=pickle.load(pickle_in)
 
 def welcome():
     return "Welcome All"
-# creating the function for prediction 
-def Churn_prediction (input_data):
+
+def Churn_prediction(voice_plan_yes, voice_messages, intl_plan_yes, intl_mins, intl_calls, intl_charge, day_mins, day_calls, day_charge, eve_mins, eve_calls, eve_charge, night_mins, night_calls, night_charge, customer_calls):
     
-    # changing the input_data to numpy array
-    input_data_as_numpy_array = np.asarray(input_data)
-
-    # reshape the array as we are predicting for one instance
-    input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
-
-    prediction = loaded_model.predict(input_data_reshaped)
+    prediction=loaded_model.predict([[voice_plan, voice_messages, intl_plan, intl_mins, intl_calls, intl_charge, day_mins, day_calls, day_charge, eve_mins, eve_calls, eve_charge, night_mins, night_calls, night_charge, customer_calls]])
     print(prediction)
-
     if (prediction[0] == 0):
-      return 'The person has not churned'
+        return 'The person will not be churned'
     else:
-      return 'The person has churned'
-  
+        return 'The person will be churned'
     
+
+
+
 def main():
     
-    # Title 
-    st.title('Churn Prediction Web App')
+    st.title('Churn prediction Web App')
     html_temp = """
     <div style="background-color:tomato;padding:10px">
     <h2 style="color:white;text-align:center;">Streamlit Churn Prediction ML App </h2>
     </div>
     """
+    st.markdown(html_temp,unsafe_allow_html=True)
     
-    # Getting input from user 
+    voice_plan = st.sidebar.selectbox('Voiceplan',('1','0'))
+    voice_messages = st.sidebar.number_input('Insert no of voice_message')
+    intl_plan = st.sidebar.selectbox('Intlplan',('1','0'))
+    intl_mins = st.sidebar.number_input('Insert intl mins')
+    intl_calls = st.sidebar.number_input('Insert intl calls')
+    intl_charge = st.sidebar.number_input('Insert intl charge')
+    day_mins = st.sidebar.number_input('Insert day mins')
+    day_calls = st.sidebar.number_input('Insert day calls')
+    day_charge = st.sidebar.number_input('Insert day charge')
+    eve_mins = st.sidebar.number_input('Insert eve mins')
+    eve_calls = st.sidebar.number_input('Insert eve calls')
+    eve_charge = st.sidebar.number_input('Insert eve charge')
+    night_mins = st.sidebar.number_input('Insert night mins')
+    night_calls = st.sidebar.number_input('Insert night calls')
+    night_charge = st.sidebar.number_input('Insert night charge')
+    customer_calls = st.sidebar.number_input('Insert no of customer_calls')
     
-    voice_plan = st.selectbox('Voiceplan',('1','0'))
-    voice_messages = st.number_input('Insert no of voice_message')
-    intl_plan = st.selectbox('Intlplan',('1','0'))
-    intl_mins = st.number_input('Insert intl mins')
-    intl_calls = st.number_input('Insert intl calls')
-    intl_charge = st.number_input('Insert intl charge')
-    day_mins = st.number_input('Insert day mins')
-    day_calls = st.number_input('Insert day calls')
-    day_charge = st.number_input('Insert day charge')
-    eve_mins = st.number_input('Insert eve mins')
-    eve_calls = st.number_input('Insert eve calls')
-    eve_charge = st.number_input('Insert eve charge')
-    night_mins = st.number_input('Insert night mins')
-    night_calls = st.number_input('Insert night calls')
-    night_charge = st.number_input('Insert night charge')
-    customer_calls = st.number_input('Insert no of customer_calls')
+    #code for prediction (the result of prediction will return in this empty string)
+    Churn = ''
     
-    
-    
-    # Code For Prediction 
-    Result = ''
-    
-    # Creating a Button For Prediction 
+    #creating button for prediction
     if st.button('Churn Result'):
-        Result = Churn_prediction([voice_plan,voice_messages,intl_plan,intl_mins,intl_calls,intl_charge,day_mins,day_calls,day_charge,eve_mins,eve_calls,eve_charge,night_mins,night_calls,night_charge,customer_calls])
-        
-        
-    st.success(Result)
+        Churn = Churn_prediction(voice_plan_yes, voice_messages, intl_plan_yes, intl_mins, intl_calls, intl_charge, day_mins, day_calls, day_charge, eve_mins, eve_calls, eve_charge, night_mins, night_calls, night_charge, customer_calls)
+    
+    
+    st.success(Churn)
     
     
     
 if __name__ == '__main__':
-    main() 
+    main()
+
